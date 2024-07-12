bfa3a1ed28d2b5325d4891081b99f5babb187171
mahbub <mahbub.shuvo10@gmail.com>
2024-07-12 10:43:17 +0600

    Desgin About Page

---
+++ resources/views/about/about.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.master')
+
+@section('title')
+	<title>About | Naysabd</title>
+	<meta name="keywords" content="">
+	<meta name="author" content="">
+	<meta name="description" content="">
+@endsection
+
+@section('banner')
+	<!--banner-->
+	<div class="banner1">
+		<div class="container">
+		</div>
+	</div>
+@endsection
+
+@section('content')
+
+		<div class="products-agileinfo">
+			<h2 class="tittle">Our Fashion Service</h2>
+			<div class="container">
+				<div class="product-agileinfo-grids w3l">
+					<div class="col-md-12 product-agileinfon-grid1 w3l">
+						<div class="product-agileinfon-top">
+							<div class="col-md-12 product-agileinfon-grid w31">
+								<p class="text-justify">For the fashion industry to be sustainable economically, it must be sustainable socially and environmentally too. These provocative scenarios challenge all of us to look beyond the short term and use our collective power to work to create the kind of positive world we’d like to see in 2025. To make this idea true, N.M Fashion is playing its vital role in garments exports and fashion industry. We are a trusted cloth supplier in Bangladesh and the top prior garment buying company in Bangladesh. N.M Fashion is named as one of the leading buying factory in Bangladesh, being the prominent T-shirt supplier, Polo shirt supplier, Formal Shirt supplier, Under Garments supplier, Ladies shirt supplier, Gent shirt supplier, Ladies pant supplier, Home Textile suppler and Terry Towel Suppler.</p><br>
+								<p class="text-justify">We believe that only by providing the Customers with the Services and Products specified at the requested time, and in the stated manner, then only we can claim to be a Quality Conscious Company, and expect the Customer's continued patronage. Therefore we as a company must at all times ensure that we all participate in producing our products in accordance with the customer's requirements.</p><br>
+								<p class="text-justify">We serve in Woven and Knit garments to our valued local and international clients. Our production department consists of experienced and creative staff members. All are fully devoted to offer their services in their respective fields and departments. Our team always accepts the comments of the customers and reply to the queries. We believe in having motivating and positive minded staff members, who can give us their valuable suggestions to make the garments and outfits more dashing.</p><br />
+								<p class="text-justify">The range offered by us is competitively priced and all consignments are delivered to the buyers well within the decided time. This has always remained the ultimate mission of the company. Every enquiry or order is attended to with utmost sincerity so that our customers get the satisfaction that their money has been worth the investment made in our products.</p><br />
+								<p class="text-justify">The focus of our Merchandising department is to create unique and innovative samples, so that we can give cent percent accuracy to our clients. Our technical merchandisers are a source of getting the best quality as they are always ready to assist the customers in case of any confusion. Timely delivery of the consignments is always upheld under all circumstances. We have entered in business collaboration with leading shipping agents to ensure that the consignments are dispatched and received on time. The entire procedure of getting prior approval for pre and post shipments is efficiently done by our expert professionals. </p><br />
+								<p class="text-justify">We have a vast team of friendly and well mannered persons. The Quality Assurance Department has always been ready to keep an eye on every item we produce and make sure that the outfits the production departments produces is up to the international standards and requirements. In a single line we say and believe that Time, Quality and the Satisfaction of the Customers and Buyers is what has made us the top leading company of Bangladesh. So, we are proud to be the best buying factory of Bangladesh.</p>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>		
+			
+@endsection
