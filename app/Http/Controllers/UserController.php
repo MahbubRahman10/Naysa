a6639eb3e77982795229471dfdc43eeae7d6efe4
mahbub <mahbub.shuvo10@gmail.com>
2024-08-01 22:08:58 +0200

    Define User Controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+use App\Order;
+use App\User;
+use App\Wishlist;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends BaseController
+{
+ 	
+
+
+
+
+}
