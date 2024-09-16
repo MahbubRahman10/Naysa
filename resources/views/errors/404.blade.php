21f7bde56105a3715fab9d494c44fe9f84362ebe
mahbub <mahbub.shuvo10@gmail.com>
2024-09-16 17:24:15 +0200

    Added Error UI

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>404 Error</title>
+    <style>
+        
+h2{
+    text-align:center;
+    font-size:100px; 
+    font-variant-numeric: oldstyle-nums stacked-fractions;
+}
+
+h4{
+    text-align:center;
+    font-size:40px; 
+}
+
+p{
+
+    text-align:center;
+    font-size:30px;
+
+}
+
+h6{
+    text-align:center;
+    font-size:30px;
+}
+a{
+    text-decoration: underline;
+}
+
+
+    </style>
+</head>
+<body>
+    <div>
+        <h2>404</h2>
+        <h4>Page not found</h4>
+        {{-- <p>You broke the balance of the internet</p> --}}
+        <h6> <a href="/">Go to back</a></h6>
+    </div>
+</body>
+</html>
